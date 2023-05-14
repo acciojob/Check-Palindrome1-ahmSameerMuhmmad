a60661c0cdea593dcc7ab115be6ba2842cc8792b
Muhmmad Sameer <120492305+ahmSameerMuhmmad@users.noreply.github.com>
2023-05-14 19:50:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-  for(let i=0; i<str.length/2; i++){
-	  if(str[i]!=str[str.length-i-1]){
-		  console.log("false");
-	  }
+   for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
   }
-	console.log("true");
+  
+  return true;
 }
 module.exports = palindrome
